9d049717040d266fcef6c0603e47af3182b1f270
Nitesh Sharma <niteshsharmaniteshsharma856@gmail.com>
2026-04-16 17:32:04 +0530

    fixed cors for vercel

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
-const cookieParser = require("cookie-parser")
-const DB = require("./config/DataBae.js")
+const cookieParser = require("cookie-parser");
+const DB = require("./config/DataBae.js");
 const userRoute = require("./routes/userRoute.js");
-const chatRoutes=require("./routes/Chat.js");
-
+const chatRoutes = require("./routes/Chat.js");
 
 const port = process.env.PORT;
 const app = express();
@@ -14,17 +13,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin: "http://localhost:5173",
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://https-lnkd-in-d-f4zs-mzj.vercel.app"
+    ],
     credentials: true
-}));
- 
+  })
+);
 
 app.use("/api/auth", userRoute);
-app.use("/api",chatRoutes);
-
+app.use("/api", chatRoutes);
 
 app.listen(port, () => {
-    console.log("server create successfully.");
-    DB();
-})
+  console.log("server create successfully.");
+  DB();
+});
